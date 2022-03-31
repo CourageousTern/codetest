8106dd7c5a0d9a63e54d6ee77ae9f416e2b4cae1
CourageousTern <81249961+CourageousTern@users.noreply.github.com>
2022-03-31 12:44:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
+let numbers = [2, 4, 6]
 const findSum = numbers.reduce((accumulator, currentValue) => {
-  return accumulator + currentValue
-});
+    return accumulator + currentValue
+  });
 
 const findFrequency = function(array) {
-  // your code here - don't forget to return an object!
-};
-
+    // your code here - don't forget to return an object!
+  };
+  
 const isPalindrome = function(str) {
-    var re = /[\W_]/g;
-    var lowRegStr = str.toLowerCase().replace(re, '');
-    var reverseStr = lowRegStr.split('').reverse().join(''); 
-    return reverseStr === lowRegStr;
- };
-
+      var re = /[\W_]/g;
+      var lowRegStr = str.toLowerCase().replace(re, '');
+      var reverseStr = lowRegStr.split('').reverse().join(''); 
+      return reverseStr === lowRegStr;
+   };
+  
 const largestPair = function(array) {
-  let max = -Infinity;
-    for (let i = 1; i < array.length; i++) {
-        max = Math.max(array[i] * array[i - 1], max);
-    }
-    return max;
-};
-
+    let max = -Infinity;
+      for (let i = 1; i < array.length; i++) {
+          max = Math.max(array[i] * array[i - 1], max);
+      }
+      return max;
+  };
+  
 const removeParenth = function(str) {
-  do {
-        removeParenth = str;
-        str = str.replace(/\([^\)\(]*\)/, "");
-    } while (removeParenth != str);
-    return removeParenth
-};
-
+    do {
+          removeParenth = str;
+          str = str.replace(/\([^\)\(]*\)/, "");
+      } while (removeParenth != str);
+      return removeParenth
+  };
+  
 const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
-};
+    // your code here - don't forget to return a number!
+  };
+  
